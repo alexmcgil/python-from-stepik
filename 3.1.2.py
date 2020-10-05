09e3f2ff05bc9f7005aa43351b26bb8bfc24de4c
AlexMcGil <therycb1337@gmail.com>
2020-10-05 14:54:41 +0300

    killer for index

--- 3.1.2.py
+++ 3.1.2.py
@@ -1,8 +1,11 @@
+killeri = []
 def modify_list(l):
     le = len(l)
     for i in range(0,le):
         if l[i] % 2 != 0:
-            del l[i]
+            killeri.append(i)
+    for i in range(le, 0):
+        del (l[killeri[i]])
     le = len(l)
     for i in range(0, le):
         if l[i] % 2 == 0:
