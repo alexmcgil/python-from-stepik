382c54a309065add482a858b95c78dca7695817c
AlexMcGil <therycb1337@gmail.com>
2020-10-05 15:06:45 +0300

    repair killer

--- 3.1.2.py
+++ 3.1.2.py
@@ -3,7 +3,7 @@ def modify_list(l):
     le = len(l)
     for i in range(0,le):
         if l[i] % 2 != 0:
-            killeri.append(i)
+            killeri.append(l[i])
     for value in killeri:
         l.remove(value)
     le = len(l)
